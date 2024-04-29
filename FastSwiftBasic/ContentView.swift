45b99c20fa904c69fe4dd5d9a35a3c988a698901
“Caleb <rkffpq31@gmail.com>
2024-04-30 02:50:24 +0900

    Initial Commit

---
+++ FastSwiftBasic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FastSwiftBasic
+//
+//  Created by 주영정 on 4/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FastSwiftBasic/FastSwiftBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastSwiftBasicApp.swift
+//  FastSwiftBasic
+//
+//  Created by 주영정 on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct FastSwiftBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
